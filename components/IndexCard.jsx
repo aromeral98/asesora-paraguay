cf2c7f428b6ea252524884047e4b0b88f3fa45e0
aromeral98 <romeralalex@gmail.com>
2022-06-12 12:05:09 +0200

    Added cards new styles

--- components/IndexCard.jsx
+++ components/IndexCard.jsx
@@ -3,26 +3,18 @@ import Link from "next/link";
 
 export default function IndexCard({ title, description, href, src }) {
     return (
-        <div className="p-5 rounded-xl shadow-lg bg-white">
-            <div className="">
-                <picture >
-                    <Image width={576} height={432} src={src} alt="Trámites en el consulado" className="rounded-xl" />
-                </picture>
+        <article className='card mx-auto' style={{ maxWidth: 576 }}>
+            <Image width={576} height={432} src={src} alt="Trámites en el consulado" className="card__img" />
+            <div className="absolute z-50 top-0 rounded card__img flex justify-center">
+                <h2 className='text-secondaryColor absolute bottom-10 mx-auto bg-white w-fit p-2 px-5 border-2 border-secondaryColor' style={{ borderRadius: '12px' }}>{title}</h2>
             </div>
-
-            <div className="flex flex-col space-y-5 px-5 pt-5">
-                <h3 className="text-dark text-2xl font-medium text-secondaryColor">
-                    {title}
-                </h3>
-                <p className="lg:text-lg leading-relaxed flex-grow">
-                    {description}
-                </p>
-                <Link passHref href={href}>
-                    <button className="text-primaryColor hover:text-secondaryColor hover:underline -mr-4 rounded-2xl border-2 p-2 px-4 hover:border-secondaryColor border-primaryColor decoration-primary items-center font-medium justify-end buttonTransition self-end flex flex-row" >Ver más
-                    <svg className="ml-2" width="24" height="20" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
-                    </button>
-                </Link>
-            </div>
-        </div>
+            <Link passHref href={href}>
+                <div className='card__data'>
+                    <h2 className='card__title text-secondaryColor'>{title}</h2>
+                    <p className='card__description'>{description}</p>
+                    <button className='card__btn absolute bottom-6 right-6'>Ver más ...</button>
+                </div>
+            </Link>
+        </article>
     )
 }
