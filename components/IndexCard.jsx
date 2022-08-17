dcc98acbde405abc18c2c2fe8883af85a71be414
aromeral98 <romeralalex@gmail.com>
2022-08-17 21:10:41 +0200

    Performance changes, animation card changed

--- components/IndexCard.jsx
+++ components/IndexCard.jsx
@@ -5,11 +5,11 @@ export default function IndexCard({ title, description, href, src }) {
     return (
         <Link passHref href={href}>
         <article className='card mx-auto content cursor-pointer' style={{ maxWidth: 576 }}>
-             <div class='content-overlay z-10' />
+             <div className='content-overlay z-10' />
              <Image width={576} height={432} src={src} alt="Trámites en el consulado" className="card__img" />
-            <div class='content-details fadeIn-bottom text-white w-9/12 z-20'>
-              <h3 class='uppercase text-2xl mb-5'>{title}</h3>
-              <p class='content-text text-md font-thin'>{description}</p>
+            <div className='content-details fadeIn-bottom text-white w-9/12 z-20'>
+              <h3 className='uppercase text-2xl mb-5'>{title}</h3>
+              <p className='content-text text-md font-thin'>{description}</p>
             </div>
         </article>
         </Link> 
