d6f6ca3fbfc38033e07fd7fe8fc3a3484fa48ff6
aromeral98 <romeralalex@gmail.com>
2022-08-17 22:01:08 +0200

    Installed next analyzer

--- components/IndexCard.jsx
+++ components/IndexCard.jsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function IndexCard({ title, description, href, src }) {
     return (
-        <Link passHref href={href}>
         <article className='card mx-auto content cursor-pointer' style={{ maxWidth: 576 }}>
+        <Link passHref href={href}>
              <div className='content-overlay z-10' />
+        </Link> 
              <Image width={576} height={432} src={src} alt="Trámites en el consulado" className="card__img" />
             <div className='content-details fadeIn-bottom text-white w-9/12 z-20'>
               <h3 className='uppercase text-2xl mb-5'>{title}</h3>
               <p className='content-text text-md font-thin'>{description}</p>
             </div>
         </article>
-        </Link> 
     )
 }
