b313d59a99946d312fe607fe28408276b4b812f0
aromeral98 <alejandro@erestige.com>
2022-10-21 15:39:57 +0200

    Added styles and fixed behaviour

--- pages/calc.jsx
+++ pages/calc.jsx
@@ -4,21 +4,19 @@ export default function Calc()  {
     const [totalSalary, setTotalSalary] = useState(0)
     const [totalHours, setTotalHours] = useState(0)
 
-    const rateHour = '11.71'
+    const rateHour = '11.714'
 
     const sumar = () => {
         setTotalSalary(prev => parseFloat(prev) + parseFloat(rateHour))
         setTotalHours(prev => prev + 1)
         setTimeout(() => {
-        window.localStorage.setItem('total', totalSalary)
-        }, 50);
+        }, 1000);
     }
     const restar = () => {
-        setTotalSalary(prev => parseFloat(prev) - parseFloat(rateHour))
-        setTotalHours(prev => min(prev - 1, 0))
+        setTotalSalary(prev => Math.max(parseFloat(prev) - parseFloat(rateHour), 0))
+        setTotalHours(prev => Math.max(prev - 1, 0))
         setTimeout(() => {
-        window.localStorage.setItem('total', totalSalary)
-        }, 50);
+        }, 1000);
     }
 
     useEffect(() => {
@@ -32,18 +30,27 @@ export default function Calc()  {
       }
     }, [])
     useEffect(() => {
-      setTotalSalary(totalHours * parseFloat('11.71'))
+      if(totalHours && totalHours !== ''){
+        setTotalSalary(totalHours * parseFloat('11.714'))
+      }
+      else {
+        setTotalSalary(0)
+      }
+      window.localStorage.setItem('total', totalSalary)
   }, [totalHours])
     
   return (
-    <div className='flex flex-col justify-center items-center mt-10'>
-        <span> {totalSalary}</span>
-        <div className='mt-5 flex flex-row space-x-6'>
-            <button className='py-2 px-4 mx-2 bg-gray-400' onClick={()=> restar()}>-1</button>
-            <button className='py-2 px-4 mx-2 bg-gray-400' onClick={()=> sumar()}>+1</button>
+    <div className='absolute h-full w-full' style={{backgroundImage: "url('https://cdn.andro4all.com/andro4all/2022/04/maxim-berg-lnvpji4fC3c-unsplash.jpg?height=600')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+    <div className='flex flex-col justify-center items-center mt-10 px-10 py-10' style={{background:'rgba(255,255,255,0.7)'}}>
+        <span className='text-green-500 font-bold text-2xl '> {parseFloat(totalSalary).toFixed(2)} € </span>
+        <div className='flex flex-row space-x-6 my-5'>
+            <button className='py-2 px-4 bg-gray-400 rounded-lg hover:bg-gray-600 transition-all duration-500' onClick={()=> restar()}>-1</button>
+            <button className='py-2 px-4 bg-gray-400 rounded-lg hover:bg-gray-600 transition-all duration-500' onClick={()=> sumar()}>+1</button>
         </div>
-            Añadir total de horas <input onChange={(e)=> setTotalHours(parseInt(e.target.value))} type='text' placeholder="horas"></input>
-            <span>{totalHours} horas</span>
+        <div className='flex flex-row space-x-2'></div>
+            Añadir total de horas <input className='border-2 border-gray-400 my-2 flex rounded-lg' value={Math.round(totalHours)} onChange={(e)=> {if(e.target.value !== ''){setTotalHours(parseInt(e.target.value))}else {setTotalHours(0)}}} type='text' placeholder="Introduce horas"></input>
+            <span>{Math.round(totalHours)} horas</span>
      </div>
+    </div>
   )
 }
