6f4b6ecd1ae334cb728877c30c975bf6738dc9ae
aromeral98 <alejandro@erestige.com>
2022-10-21 14:42:29 +0200

    compile error

--- pages/calc.jsx
+++ pages/calc.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export const Calc = () => {
+export default function Calc()  {
     const [totalSalary, setTotalSalary] = useState(0)
     const [totalHours, setTotalHours] = useState(0)
 
