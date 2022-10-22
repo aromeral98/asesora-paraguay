bb1bba50be2523f726a2902a2f91aed44f0e9aea
aromeral98 <alejandro@erestige.com>
2022-10-22 13:12:00 +0200

    Added new functions

--- pages/calc.jsx
+++ pages/calc.jsx
@@ -79,21 +79,21 @@ export default function Calc() {
   return (
     <div className='absolute h-auto w-full' style={{ backgroundImage: "url('https://cdn.andro4all.com/andro4all/2022/04/maxim-berg-lnvpji4fC3c-unsplash.jpg?height=600')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
       <div className='flex flex-col justify-center items-center m-10 py-10' style={{ background: 'rgba(255,255,255,0.7)' }}>
-      <h2 className='font-bold text-2xl my-5'>CURRENTLY WON SALARY</h2>
+        <h2 className='font-bold text-xl my-5'>CURRENTLY WON SALARY</h2>
         <div className='flex flex-col items-center justify-center text-center'><span className='font-bold text-base lg:text-2xl'>Gross salary: <br /><span className='text-red-500'>{parseFloat(totalGrossCurrentSalary).toFixed(2)} € </span></span></div>
         <div className='flex flex-col items-center justify-center text-center'><span className='font-bold text-base lg:text-2xl'>Net salary:  <br /><span className='text-green-500'>{parseFloat(totalSalary).toFixed(2)} € </span></span></div>
-        <div className='flex flex-row space-x-6 my-5'>
+        <div className='flex flex-row space-x-6 mt-5'>
           <button className='py-2 px-4 bg-gray-400 rounded-lg hover:bg-gray-600 transition-all duration-500' onClick={() => restar()}>-1</button>
           <button className='py-2 px-4 bg-gray-400 rounded-lg hover:bg-gray-600 transition-all duration-500' onClick={() => sumar()}>+1</button>
         </div>
         Add hours <input className='border-2 border-gray-400 my-2 flex rounded-lg' value={Math.round(totalHours)} onChange={(e) => { if (e.target.value !== '') { setTotalHours(parseInt(e.target.value)) } else { setTotalHours(0) } }} type='text' placeholder="Add hours"></input>
         <span>{Math.round(totalHours)} horas</span>
-        <h2 className='font-bold text-2xl my-5'>AUTOCALCULATED SALARY</h2>
-        <h5 className='font-bold text-xl mt-3'>GROSS SALARY</h5>
+        <h2 className='font-bold text-xl mt-5'>AUTOCALCULATED SALARY</h2>
+        <h5 className='font-bold text-lg mt-3'>GROSS SALARY</h5>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Gross worked hour rate: </span><span className='text-red-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(hourGrossWorkedSalary).toFixed(2)} € </span></div>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Gross hour rate: </span><span className='text-red-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(rateGrossHour).toFixed(2)} € </span></div>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Autocalculated gross salary: </span><span className='text-red-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(autocalculatedGrossSalary).toFixed(4)} € </span></div>
-        <h5 className='font-bold text-xl mt-3'>NET SALARY</h5>
+        <h5 className='font-bold text-lg mt-3'>NET SALARY</h5>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Net worked hour rate: </span><span className='text-green-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(hourWorkedSalary).toFixed(2)} € </span></div>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Net hour rate: </span><span className='text-green-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(rateHour).toFixed(2)} € </span></div>
         <div className='flex flex-col space-x-2 items-center my-2'><span>Autocalculated net salary: </span><span className='text-green-500 font-bold text-base lg:text-2xl ml-2'> {parseFloat(autocalculatedSalary).toFixed(4)} € </span></div>
