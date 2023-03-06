2bfca87e696da569edf28131a4ec7ccfce6ed4b2
Alejandro romeral <romeralalex@gmail.com>
2023-03-06 09:30:45 -0300

    Removed image

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ReactGA from 'react-ga4'
 import { Helmet } from 'react-helmet'
-import logo from '../public/img/logo.webp'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -24,15 +23,14 @@ function MyApp({ Component, pageProps }) {
 
   return <Helmet>
     <meta property="og:title" content="Asesora Paraguay - Tramitación de residencias, nacionalidad y migración" />
-    <meta property="og:description" content="Somos un despacho que se dedica a la tramitación de residencias, nacionalidad y todo lo relacionado con el proceso de migración en Paraguay, enfocado principalmente a empresarios, inversores y nómadas digitales." />
+    {/* <meta property="og:description" content="Somos un despacho que se dedica a la tramitación de residencias, nacionalidad y todo lo relacionado con el proceso de migración en Paraguay, enfocado principalmente a empresarios, inversores y nómadas digitales." /> */}
     <meta property="og:image" content={logo} />
     <meta property="og:url" content="URL de la página a compartir" />
     <meta property="og:type" content="website" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Asesora Paraguay - Tramitación de residencias, nacionalidad y migración" />
     <meta name="twitter:description" content="Somos un despacho que se dedica a la tramitación de residencias, nacionalidad y todo lo relacionado con el proceso de migración en Paraguay, enfocado principalmente a empresarios, inversores y nómadas digitales." />
-    <meta name="twitter:image" content={logo} />
-
+    {/* <meta name="twitter:image" content={logo} /> */}
     <Component {...pageProps} />
   </Helmet>
 }
