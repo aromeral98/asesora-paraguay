ebe922a89df8a2faf0bb43c46f4b623b3c37f22f
Alejandro romeral <romeralalex@gmail.com>
2023-03-08 09:23:25 -0300

    fix

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -6,57 +6,59 @@ import ContactButton from './ContactButton'
 
 
 type Props = {
-    children: ReactNode
+  children: ReactNode
 }
 const Layout: React.FC<Props> = ({ children }) => {
-    function reveal () {
-        const reveals = document.querySelectorAll('.reveal')
-    
-        for (let i = 0; i < reveals.length; i++) {
-          const windowHeight = window.innerHeight
-          const elementTop = reveals[i].getBoundingClientRect().top
-          const elementVisible = 0
-          if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add('active')
-          } else {
-            reveals[i].classList.remove('active')
-          }
-        }
-      }
-        
-      const verifyVisibility = (entry: any )=> {
-        const contactButton = document.getElementById('contactButton')
-        if (entry[0].isIntersecting) {
-          contactButton?.classList.add('hidden')
-        } else if(!entry[0].isIntersecting) {
-          contactButton?.classList.remove('hidden')
-        }
-      }
+  function reveal() {
+    const reveals = document.querySelectorAll('.reveal')
 
-      useEffect(() => {
-        if (typeof window !== "undefined") {
-          window?.addEventListener('scroll', reveal)
-          const form: any = document.getElementById('form')
-          window?.addEventListener('touchend', ()=>{
-            const observer = new IntersectionObserver(verifyVisibility,{threshold: 0.4})
-            if(form !== null){
-              observer.observe(form)
-            }
-          })
-        }
-      }, [])
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight
+      const elementTop = reveals[i].getBoundingClientRect().top
+      const elementVisible = 0
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add('active')
+      } else {
+        reveals[i].classList.remove('active')
+      }
+    }
+  }
 
-    return (
-        <>
-            <Navbar />
-            <ContactButton />
-            <CoverImage />
-            <main className='flex flex-grow flex-col mx-auto contactPattern overflow-hidden'>
-                {children}
-            </main>
-            <Footer />
-        </>
-    )
+  const verifyVisibility = (entry: any) => {
+    const contactButton = document.getElementById('contactButton')
+    if (entry[0].isIntersecting) {
+      contactButton?.classList.add('hidden')
+    } else if (!entry[0].isIntersecting) {
+      contactButton?.classList.remove('hidden')
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window?.addEventListener('scroll', reveal)
+      const form: any = document.getElementById('form')
+      window?.addEventListener('touchend', () => {
+        const observer = new IntersectionObserver(verifyVisibility, { threshold: 0.4 })
+        if (form !== null) {
+          observer.observe(form)
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      {window && window.location.pathname !== '/'
+        ? <ContactButton />
+        : null}
+      <CoverImage />
+      <main className='flex flex-grow flex-col mx-auto contactPattern overflow-hidden'>
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
