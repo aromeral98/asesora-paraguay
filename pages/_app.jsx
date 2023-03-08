48b23ad6d99900d6febad956ec9db8ccefc8f1a3
Alejandro romeral <romeralalex@gmail.com>
2023-03-08 07:46:11 -0300

    Revert head changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,9 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import ReactGA from 'react-ga4'
-import { Helmet } from 'react-helmet'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const { pathname } = router
-
+ 
   useEffect(() => {
     ReactGA.initialize([
       {
@@ -17,22 +13,11 @@ function MyApp({ Component, pageProps }) {
 
   }, []);
 
-  useEffect(() => {
-    ReactGA.send({ hitType: "click", page: pathname });
-  }, [pathname]);
+  // useEffect(() => {
+  //   ReactGA.send({ hitType: "click", page: pathname });
+  // }, [pathname]);
 
-  return <Helmet>
-    <meta property="og:title" content="Asesora Paraguay - Tramitación de residencias, nacionalidad y migración" />
-    <meta property="og:description" content="Somos un despacho que se dedica a la tramitación de residencias, nacionalidad y todo lo relacionado con el proceso de migración en Paraguay, enfocado principalmente a empresarios, inversores y nómadas digitales." />
-    {/* <meta property="og:image" content={logo} /> */}
-    <meta property="og:url" content="URL de la página a compartir" />
-    <meta property="og:type" content="website" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="Asesora Paraguay - Tramitación de residencias, nacionalidad y migración" />
-    <meta name="twitter:description" content="Somos un despacho que se dedica a la tramitación de residencias, nacionalidad y todo lo relacionado con el proceso de migración en Paraguay, enfocado principalmente a empresarios, inversores y nómadas digitales." />
-    {/* <meta name="twitter:image" content={logo} /> */}
-    <Component {...pageProps} />
-  </Helmet>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
