bfc5e93b20ec46c225ce631c5dcb8974c8eb223c
Alejandro romeral <romeralalex@gmail.com>
2023-03-09 20:02:09 -0300

    Removed intersection observer of contact button

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -9,42 +9,7 @@ type Props = {
   children: ReactNode
 }
 const Layout: React.FC<Props> = ({ children }) => {
-  function reveal() {
-    const reveals = document.querySelectorAll('.reveal')
 
-    for (let i = 0; i < reveals.length; i++) {
-      const windowHeight = window.innerHeight
-      const elementTop = reveals[i].getBoundingClientRect().top
-      const elementVisible = 0
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add('active')
-      } else {
-        reveals[i].classList.remove('active')
-      }
-    }
-  }
-
-  const verifyVisibility = (entry: any) => {
-    const contactButton = document.getElementById('contactButton')
-    if (entry[0].isIntersecting) {
-      contactButton?.classList.add('hidden')
-    } else if (!entry[0].isIntersecting) {
-      contactButton?.classList.remove('hidden')
-    }
-  }
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window?.addEventListener('scroll', reveal)
-      const form: any = document.getElementById('form')
-      window?.addEventListener('touchend', () => {
-        const observer = new IntersectionObserver(verifyVisibility, { threshold: 0.4 })
-        if (form !== null) {
-          observer.observe(form)
-        }
-      })
-    }
-  }, [])
 
   return (
     <>
