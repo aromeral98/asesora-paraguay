31751c9f9766d2e2bb226cf8b1d969319f991546
Alejandro romeral <romeralalex@gmail.com>
2023-03-11 09:53:25 -0300

    Added new key translations

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,19 +1,22 @@
+import { useTranslation } from 'react-i18next'
 import Countries from './helpers/Countries'
 
 export default function ContactForm({ bigTitle = false }) {
+  const { t } = useTranslation('global')
+
   return (
     <div className="md:col-span-2 mx-auto bg-white">
       <form
-      id='form'
-        action="https://formspree.io/f/xbjwpkpo" 
+        id='form'
+        action="https://formspree.io/f/xbjwpkpo"
         method="POST" className="w-full lg:w-full mx-auto">
         <div className="shadow overflow-hidden sm:rounded-md">
           <div className="px-4 py-5 sm:px-10 sm:pb-6 sm:pt-0">
-            <p className={((bigTitle) ? 'text-4xl' : 'text-2xl') +  " leading-6 text-secondaryColor my-5 text-center"}>Formulario de contacto</p>
+            <p className={((bigTitle) ? 'text-4xl' : 'text-2xl') + " leading-6 text-secondaryColor my-5 text-center"}>{t('contact.form.title')}</p>
             <div className="grid grid-cols-12 gap-6">
               <div className="col-span-12">
                 <label htmlFor="first-name" className="block font-medium text-gray-700">
-                  Nombre
+                  {t('contact.form.name')}
                 </label>
                 <input
                   type="text"
@@ -26,7 +29,7 @@ export default function ContactForm({ bigTitle = false }) {
               </div>
               <div className="col-span-12">
                 <label htmlFor="email-address" className="block font-medium text-gray-700">
-                  Email
+                  {t('contact.form.email')}
                 </label>
                 <input
                   type="text"
@@ -39,9 +42,9 @@ export default function ContactForm({ bigTitle = false }) {
               </div>
 
               <div className="col-span-12 styled-select relative">
-              <svg className='right-4 absolute top-10'  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+                <svg className='right-4 absolute top-10' xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
                 <label htmlFor="country" className="block font-medium text-gray-700">
-                  Pais
+                  {t('contact.form.country')}
                 </label>
                 <select
                   id="country"
@@ -59,7 +62,7 @@ export default function ContactForm({ bigTitle = false }) {
               </div>
               <div className="col-span-12">
                 <label htmlFor="phone" className="block font-medium text-gray-700">
-                  Telefono
+                  {t('contact.form.phone')}
                 </label>
                 <input
                   type="text"
@@ -71,9 +74,9 @@ export default function ContactForm({ bigTitle = false }) {
                 />
               </div>
               <div className="col-span-12 styled-select relative">
-              <svg className='right-4 absolute top-10'  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+                <svg className='right-4 absolute top-10' xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
                 <label htmlFor="response-method" className="block font-medium text-gray-700">
-                  ¿Como quieres recibir tu respuesta?
+                  {t('contact.form.howtoreceiveresponse')}
                 </label>
                 <select
                   id="response-method"
@@ -81,15 +84,15 @@ export default function ContactForm({ bigTitle = false }) {
                   autoComplete="response-method"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primaryColor pl-3 focus:border-primaryColor  sm:text-sm cursor-pointer"
                 >
-                  <option value='phone'>Llamada</option>
-                  <option value='email'>Email</option>
-                  <option value='whatsapp'>Whatsapp</option>
+                  <option value='phone'>{t('contact.form.call')}</option>
+                  <option value='email'>{t('contact.form.email')}</option>
+                  <option value='whatsapp'>{t('contact.form.whatsapp')}</option>
                 </select>
               </div>
               <div className="col-span-12 styled-select relative">
-              <svg className='right-4 absolute top-10'  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+                <svg className='right-4 absolute top-10' xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
                 <label htmlFor="topic" className="block font-medium text-gray-700">
-                  Asunto
+                  {t('contact.form.affair')}
                 </label>
                 <select
                   id="topic"
@@ -97,17 +100,17 @@ export default function ContactForm({ bigTitle = false }) {
                   autoComplete="topic"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primaryColor pl-3 focus:border-primaryColor  sm:text-sm cursor-pointer"
                 >
-                  <option value='Certificados'>Certificados</option>
-                  <option value='Ciudadania'>Ciudadania</option>
-                  <option value='Residencia'>Residencia</option>
-                  <option value='Tramites'>Tramites</option>
-                  <option value='Consulta'>Consulta</option>
-                  <option value='Otros'>Otros</option>
+                  <option value='Certificados'>{t('contact.form.certificate')}</option>
+                  <option value='Ciudadania'>{t('contact.form.citizenship')}</option>
+                  <option value='Residencia'>{t('contact.form.residence')}</option>
+                  <option value='Tramites'>{t('contact.form.procedures')}</option>
+                  <option value='Consulta'>{t('contact.form.consultation')}</option>
+                  <option value='Otros'>{t('contact.form.others')}</option>
                 </select>
               </div>
               <div className="col-span-12 gap-6">
                 <label htmlFor="message" className="block font-medium text-gray-700">
-                  Consulta
+                  {t('contact.form.message')}
                 </label>
                 <textarea
                   rows={10}
@@ -125,7 +128,9 @@ export default function ContactForm({ bigTitle = false }) {
               className="inline-flex justify-center px-4 border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primaryColor hover:bg-white hover:text-secondaryColor
               transition-all duration-300 hover:border-secondaryColor focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondaryColor border py-2"
             >
-              <strong>Contacto</strong>
+              <strong>                
+                {t('contact.form.contact')}
+              </strong>
             </button>
           </div>
         </div>
