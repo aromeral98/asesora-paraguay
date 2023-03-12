9e9fd04ef22c3bfbb022d4c53b11f3f0b711160d
Alejandro romeral <romeralalex@gmail.com>
2023-03-12 14:12:01 -0300

    Article deleted unused info

--- pages/article/[id].jsx
+++ pages/article/[id].jsx
@@ -5,18 +5,30 @@ import { articles } from '../../articles'
 import Layout from '../../components/shared/Layout'
 import ShareBar from '../../components/shared/ShareBar'
 import { jsonLDTypes } from '../../jsonLD/types/jsonLDTypes'
+import advantages from '../../articles/advantages.json'
+import { useRouter } from 'next/router'
 
-export default function Article({ title = 'Ventajas fiscales para freelancers en Paraguay | Bajos impuestos y facilidades para emprendedores', article }) {
+export default function Article({ title, article }) {
   const { t } = useTranslation('global')
   const seoDescription = "Descubre por qué Paraguay es un destino atractivo para freelancers y autónomos. Conoce las ventajas fiscales que ofrece el país, como bajos impuestos, facilidades para emprender cualquier tipo de negocio y mano de obra barata. Además, con un clima favorable y una economía bancarizada, Paraguay es el lugar ideal para trabajar y hacer negocios."
-  
-  const generateParagraph = (article) => {
-    console.log(articles)
-  }
   useEffect(() => {
-    generateParagraph()
+    console.log(article)
   }, [])
-  
+
+  // function renderArticle(article) {
+  //   const html = [];
+  //   for (let i = 1; i <= article.length; i++) {
+  //     const paragraph = article[i];
+  //     if (paragraph) {
+  //       const paragraphHtml = paragraph.replace(/<strong>(.*?)<\/strong>/g, '<strong>$1</strong>');
+  //       html.push(
+  //         <p key={i} className="text-dark lg:text-lg" dangerouslySetInnerHTML={{ __html: paragraphHtml }} />
+  //       );
+  //     }
+  //   }
+  //   return <>{html}</>
+  // }
+
   return (
     <Layout>
       <Helmet>
@@ -44,27 +56,10 @@ export default function Article({ title = 'Ventajas fiscales para freelancers en
             {title}
           </h2>
           <div className="space-y-4 lg:space-y-8">
-            {/* <h5 className="text-lg lg:text-xl font-bold leading-tight lg:leading-relaxed">Ventajas fiscales para freelancers y emprendedores en Paraguay</h5> */}
-            <p className="text-dark lg:text-lg">
-            Paraguay es uno de los países más atractivos para los negocios en América Latina, gracias a su política de <strong>bajos impuestos</strong> y su facilidad para hacer negocios. Esto lo hace especialmente interesante para aquellos <strong>freelancers</strong> y <strong>autónomos</strong> que buscan establecer su negocio en un entorno económico favorable.
-            </p>
             <h5 className="text-lg lg:text-xl font-bold leading-tight lg:leading-relaxed">Impuestos bajos y exenciones para freelancers</h5>
             <p className="text-dark lg:text-lg">
               Una de las principales ventajas fiscales que ofrece Paraguay es su IVA del 10%. Si eres freelancer y vendes servicios o productos dentro del país, deberás pagar solamente el 10% de impuesto al valor agregado. Pero si tus servicios son para clientes fuera del país, estarás exento de pagar este impuesto.
             </p>
-            <p className="text-dark lg:text-lg">
-              Además, el impuesto sobre la renta también es bastante bajo en Paraguay. Los freelancers y emprendedores pagan solamente el 10% de impuesto sobre la renta, lo que representa una tasa muy baja en comparación con otros países de la región. Y si decides abrir una <strong>LLC</strong> en Estados Unidos, también estarás exento de pagar impuestos, ya que los dividendos que provengan de empresas extranjeras no pagan impuestos en Paraguay.
-            </p>
-            <h5 className="text-lg lg:text-xl font-bold leading-tight lg:leading-relaxed">Clima favorable para los negocios en Paraguay</h5>
-            <p className="text-dark lg:text-lg">
-              Otra de las ventajas de Paraguay es su clima económico favorable para los negocios. El país cuenta con una mano de obra barata y una gran facilidad para hacer contactos empresariales. Además, las restricciones para establecer negocios son mucho menores que en Europa, lo que hace que sea un destino atractivo para emprendedores de todo tipo.
-            </p>
-            <h5 className="text-lg lg:text-xl font-bold leading-tight lg:leading-relaxed">Infraestructura bancaria y facilidades para pagar</h5>
-            <p className="text-dark lg:text-lg">
-              En cuanto a la infraestructura bancaria, Paraguay está bastante bancarizado, lo que hace que sea fácil y conveniente pagar con tarjeta y encontrar cajeros automáticos en casi cualquier lugar, incluso en los supermercados.
-            </p>
-            <p className="text-dark lg:text-lg">
-              En conclusión, si eres freelancer o emprendedor y buscas un entorno económico favorable para establecer tu negocio, Paraguay es una excelente opción. Con su <strong>bajo impuesto al valor agregado y al impuesto sobre la renta</strong>, así como su clima económico favorable para los negocios y su infraestructura bancaria, este país es un destino atractivo para aquellos que buscan establecer su negocio en América Latina. </p>
           </div>
         </section>
         <div className="flex justify-end my-8">
@@ -73,4 +68,22 @@ export default function Article({ title = 'Ventajas fiscales para freelancers en
       </article>
     </Layout>
   )
+}
+
+// Generates `/posts/1` and `/posts/2`
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: 'advantages' } }, 
+      { params: { id: '22442' } }],
+    fallback: false, // can also be true or 'blocking'
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  const filePath = `../../articles/${id}.json`;
+  const article = filePath
+  
+  return { props: article };
 }
