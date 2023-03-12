3a9b18db8208a93f7fa94ffb58bcd7a1b2da29f8
Alejandro romeral <romeralalex@gmail.com>
2023-03-12 15:16:45 -0300

    Added image with explicit height width and alt

--- pages/article/[id].jsx
+++ pages/article/[id].jsx
@@ -5,32 +5,35 @@ import { articles } from '../../articles'
 import Layout from '../../components/shared/Layout'
 import ShareBar from '../../components/shared/ShareBar'
 import { jsonLDTypes } from '../../jsonLD/types/jsonLDTypes'
+import image from '../../public/img/articles/advantages/0.webp'
 
 export default function Article({
   article = {
     "seo": {
-      "title": "Ventajas fiscales para freelancers en Paraguay | Bajos impuestos y facilidades para emprendedores",
-      "description": "Descubre por qué Paraguay es atractivo para freelancers y autónomos. Bajos impuestos, facilidades para emprender y mano de obra barata. Clima favorable y economía bancarizada. El lugar ideal para trabajar y hacer negocios. #Paraguay #Freelancers #Negocios",
-      "keywords": "Paraguay, freelancers, autónomos, bajos impuestos, facilidades para emprendedores, mano de obra barata"
+        "title": "Ventajas fiscales para freelancers en Paraguay | Bajos impuestos y facilidades para emprendedores",
+        "description": "Descubre por qué Paraguay es atractivo para freelancers y autónomos. Bajos impuestos, facilidades para emprender y mano de obra barata. Clima favorable y economía bancarizada. El lugar ideal para trabajar y hacer negocios. #Paraguay #Freelancers #Negocios",
+        "keywords": "Paraguay, freelancers, autónomos, bajos impuestos, facilidades para emprendedores, mano de obra barata"
     },
     "article": {
-      "image": {
-        "src": "https://www.bancomundial.org/content/dam/photos/780x439/2019/feb-1/PY-Asuncion.jpg",
-        "width": 780,
-        "height": 437
-      },
-      "paragraph": "Paraguay es uno de los países más atractivos para los negocios en América Latina, gracias a su política de <strong>bajos impuestos</strong> y su facilidad para hacer negocios. Esto lo hace especialmente interesante para aquellos <strong>freelancers</strong> y <strong>autónomos</strong> que buscan establecer su negocio en un entorno económico favorable.",
-      "title1": "Impuestos bajos y exenciones para freelancers",
-      "paragraph1": "Una de las principales ventajas fiscales que ofrece Paraguay es su IVA del 10%. Si eres freelancer y vendes servicios o productos dentro del país, deberás pagar solamente el 10% de impuesto al valor agregado. Pero si tus servicios son para clientes fuera del país, estarás exento de pagar este impuesto.",
-      "paragraph1.1": "Además, el impuesto sobre la renta también es bastante bajo en Paraguay. Los freelancers y emprendedores pagan solamente el 10% de impuesto sobre la renta, lo que representa una tasa muy baja en comparación con otros países de la región. Y si decides abrir una <strong>LLC</strong> en Estados Unidos, también estarás exento de pagar impuestos, ya que los dividendos que provengan de empresas extranjeras no pagan impuestos en Paraguay.",
-      "title2": "Clima favorable para los negocios en Paraguay",
-      "paragraph2": "Otra de las ventajas de Paraguay es su clima económico favorable para los negocios. El país cuenta con una mano de obra barata y una gran facilidad para hacer contactos empresariales. Además, las restricciones para establecer negocios son mucho menores que en Europa, lo que hace que sea un destino atractivo para emprendedores de todo tipo.",
-      "title3": "Infraestructura bancaria y facilidades para pagar",
-      "paragraph3": "En cuanto a la infraestructura bancaria, Paraguay está bastante bancarizado, lo que hace que sea fácil y conveniente pagar con tarjeta y encontrar cajeros automáticos en casi cualquier lugar, incluso en los supermercados.",
-      "paragraph3.1": "En conclusión, si eres freelancer o emprendedor y buscas un entorno económico favorable para establecer tu negocio, Paraguay es una excelente opción. Con su <strong>bajo impuesto al valor agregado y al impuesto sobre la renta</strong>, así como su clima económico favorable para los negocios y su infraestructura bancaria, este país es un destino atractivo para aquellos que buscan establecer su negocio en América Latina."
+        "image": {
+            "src": "https://www.bancomundial.org/content/dam/photos/780x439/2019/feb-1/PY-Asuncion.jpg",
+            "width": 780,
+            "height": 437,
+            "alt": "Asuncion landscape of the city"
+        },
+        "paragraph": "Paraguay es uno de los países más atractivos para los negocios en América Latina, gracias a su política de <strong>bajos impuestos</strong> y su facilidad para hacer negocios. Esto lo hace especialmente interesante para aquellos <strong>freelancers</strong> y <strong>autónomos</strong> que buscan establecer su negocio en un entorno económico favorable.",
+        "title1": "Impuestos bajos y exenciones para freelancers",
+        "paragraph1": "Una de las principales ventajas fiscales que ofrece Paraguay es su IVA del 10%. Si eres freelancer y vendes servicios o productos dentro del país, deberás pagar solamente el 10% de impuesto al valor agregado. Pero si tus servicios son para clientes fuera del país, estarás exento de pagar este impuesto.",
+        "paragraph1.1": "Además, el impuesto sobre la renta también es bastante bajo en Paraguay. Los freelancers y emprendedores pagan solamente el 10% de impuesto sobre la renta, lo que representa una tasa muy baja en comparación con otros países de la región. Y si decides abrir una <strong>LLC</strong> en Estados Unidos, también estarás exento de pagar impuestos, ya que los dividendos que provengan de empresas extranjeras no pagan impuestos en Paraguay.",
+        "title2": "Clima favorable para los negocios en Paraguay",
+        "paragraph2": "Otra de las ventajas de Paraguay es su clima económico favorable para los negocios. El país cuenta con una mano de obra barata y una gran facilidad para hacer contactos empresariales. Además, las restricciones para establecer negocios son mucho menores que en Europa, lo que hace que sea un destino atractivo para emprendedores de todo tipo.",
+        "title3": "Infraestructura bancaria y facilidades para pagar",
+        "paragraph3": "En cuanto a la infraestructura bancaria, Paraguay está bastante bancarizado, lo que hace que sea fácil y conveniente pagar con tarjeta y encontrar cajeros automáticos en casi cualquier lugar, incluso en los supermercados.",
+        "paragraph3.1" : "En conclusión, si eres freelancer o emprendedor y buscas un entorno económico favorable para establecer tu negocio, Paraguay es una excelente opción. Con su <strong>bajo impuesto al valor agregado y al impuesto sobre la renta</strong>, así como su clima económico favorable para los negocios y su infraestructura bancaria, este país es un destino atractivo para aquellos que buscan establecer su negocio en América Latina."
     }
-  }
+}
 }) {
+  console.log(image)
   const { t } = useTranslation('global')
 
   function renderArticle() {
@@ -42,7 +45,7 @@ export default function Article({
       } else if (key.startsWith('paragraph')) {
         html += `<p class="text-dark lg:text-lg">${value}</p>`;
       } else if (key.startsWith('image')) {
-        html += `<img src=${value.src} style={{height: ${value.height}, width: ${value.width}}}/>`;
+        html += `<img src=${value.src} alt=${value.alt} style={{height: ${value.height}, width: ${value.width}}}/>`;
       }
     }
 
