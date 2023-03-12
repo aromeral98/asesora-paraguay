6b0ce62aacc568e8bacd8d1a8f037b5ee16cbd58
Alejandro romeral <romeralalex@gmail.com>
2023-03-12 15:59:29 -0300

    Added json ld for article

---
+++ jsonLD/types/articlesJSONLD.js
@@ -0,0 +1,24 @@
+export const articlesJSONLD = {
+    advantages: [
+        {
+            "@context": "https://schema.org/",
+            "@type": "Article",
+            "name": "Ventajas fiscales para freelancers en Paraguay",
+            "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": "https://asesoraparaguay.com/advantages"
+            },
+            "headline": "Ventajas fiscales para freelancers en Paraguay",
+            "description": "Descubre por qué Paraguay es atractivo para freelancers y autónomos. Bajos impuestos, facilidades para emprender y mano de obra barata. Clima favorable y economía bancarizada. El lugar ideal para trabajar y hacer negocios. #Paraguay #Freelancers #Negocios",
+            "image": {
+              "@type": "ImageObject",
+              "url": "https://www.bancomundial.org/content/dam/photos/780x439/2019/feb-1/PY-Asuncion.jpg",
+              "width": 780,
+              "height": 437,
+              "description": "Asuncion landscape of the city"
+            },
+            "datePublished": "2023-03-12",
+            "dateModified": "2023-03-12",
+        }
+    ]
+}
