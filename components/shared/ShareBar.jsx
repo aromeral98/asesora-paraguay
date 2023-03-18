fc7a7f9b2ecfc33e3bccc4ff413d58881fa339f6
Alejandro romeral <romeralalex@gmail.com>
2023-03-18 10:12:06 -0300

    Updated share buttons and sitemap

--- components/shared/ShareBar.jsx
+++ components/shared/ShareBar.jsx
@@ -2,13 +2,13 @@ import { FacebookIcon, TelegramIcon, TwitterIcon, TwitterShareButton, WhatsappIc
 
 export default function ShareBar({title = '', description = "We offer advisory services for residency processing in Paraguay to foreigners, especially for digital nomads and freelancers who want to pay less taxes."}) {
     return <div className="flex flex-row space-x-4">
-        <a aria-label={'Share on facebook'} href={"https://www.facebook.com/sharer/sharer.php?u=" + (typeof window !== 'undefined' && window.location.href)} rel="noreferrer" target="_blank" data-action="share/facebook/share">
+        <a aria-label={'Share on facebook'} href={"https://www.facebook.com/sharer/sharer.php?u=https://www.asesoraparaguay.com"} rel="noreferrer" target="_blank" data-action="share/facebook/share">
         <FacebookIcon className="cursor-pointer icon" round borderRadius={25} size={45} />
         </a>
-        <a aria-label={'Share on whatsapp'} href={"whatsapp://send?text=" + (typeof window !== 'undefined' && window.location.href)} rel="noreferrer" target="_blank" data-action="share/whatsapp/share">
+        <a aria-label={'Share on whatsapp'} href={"whatsapp://send?text=https://www.asesoraparaguay.com"} rel="noreferrer" target="_blank" data-action="share/whatsapp/share">
         <WhatsappIcon className="cursor-pointer icon" round borderRadius={25} size={45} />
         </a>
-        <a aria-label={'Share on twitter'} href={`https://twitter.com/intent/tweet?text=${(typeof window !== 'undefined' && window.location.href)}`} rel="noreferrer" target="_blank" data-size="large">
+        <a aria-label={'Share on twitter'} href={`https://twitter.com/intent/tweet?text=https://www.asesoraparaguay.com`} rel="noreferrer" target="_blank" data-size="large">
             <TwitterIcon className="cursor-pointer icon" round borderRadius={25} size={45} >
         </TwitterIcon>
         </a>
