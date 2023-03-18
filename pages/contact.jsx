624a249245b958b465a15ba7a1609e72ee93c06c
Alejandro romeral <romeralalex@gmail.com>
2023-03-18 16:44:50 -0300

    translated contact meta tags and updated sitemap

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -7,18 +7,18 @@ export default function Contact() {
   return (
     <>
       <Head>
-        <title>Contacto | Asesora Paraguay - Tu asesor migratorio</title>
-        <meta name="description" content="Contáctanos para cualquier duda o pregunta relacionada con nuestros servicios de asesoramiento migratorio. Rellena el formulario de contacto y te responderemos lo antes posible." />
-        <meta name="keywords" content="Contacto, Asesora Paraguay, Asesoramiento migratorio, Preguntas frecuentes, Formulario de contacto" />
+      <title>Contact | Asesora Paraguay - Your Migration Advisor</title>
+        <meta name="description" content="Contact us for any questions or inquiries related to our migration advisory services. Fill out the contact form and we will respond as soon as possible." />
+        <meta name="keywords" content="Contact, Asesora Paraguay, Migration advisory, FAQ, Contact form" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Asesora Paraguay - Despacho especializado en migración y tramitación de residencias y nacionalidad" />
-        <meta property="og:description" content="Somos un despacho especializado en migración y tramitación de residencias y nacionalidad en Paraguay. Brindamos asesoría a empresarios, inversores y nómadas digitales en todo momento del proceso." />
+        <meta property="og:title" content="Asesora Paraguay - Specialized in migration and processing of residencies and nationality" />
+        <meta property="og:description" content="We are a specialized firm in migration and processing of residencies and nationality in Paraguay. We provide advice to entrepreneurs, investors and digital nomads at every step of the process." />
         <meta property="og:image" content="https://asesoraparaguay.com/img/articles/advantages/0.webp" />
         <meta property="og:url" content="https://asesoraparaguay.com" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Asesora Paraguay - Despacho especializado en migración y tramitación de residencias y nacionalidad" />
-        <meta name="twitter:description" content="Somos un despacho especializado en migración y tramitación de residencias y nacionalidad en Paraguay. Brindamos asesoría a empresarios, inversores y nómadas digitales en todo momento del proceso." />
+        <meta name="twitter:title" content="Asesora Paraguay - Specialized in migration and processing of residencies and nationality" />
+        <meta name="twitter:description" content="We are a specialized firm in migration and processing of residencies and nationality in Paraguay. We provide advice to entrepreneurs, investors and digital nomads at every step of the process." />
         <meta name="twitter:image" content="https://asesoraparaguay.com/img/articles/advantages/0.webp" />
         <meta name="twitter:url" content="https://asesoraparaguay.com" />
         <script type="application/ld+json">
