86490cb3c18bae4ea7167e31d47d2c4899063cc2
Alejandro romeral <romeralalex@gmail.com>
2023-03-18 09:51:57 -0300

    Added share buttons

--- components/shared/ShareBar.jsx
+++ components/shared/ShareBar.jsx
@@ -1,13 +1,22 @@
-import { FacebookIcon, TwitterIcon, TwitterShareButton, WhatsappIcon } from "react-share";
+import { FacebookIcon, TelegramIcon, TwitterIcon, TwitterShareButton, WhatsappIcon } from "react-share";
 
-export default function ShareBar({title, description}) {
+export default function ShareBar({title, description = "We offer advisory services for residency processing in Paraguay to foreigners, especially for digital nomads and freelancers who want to pay less taxes."}) {
     return <div className="flex flex-row space-x-4">
-        <FacebookIcon className="cursor-pointer icon" onClick={()=> alert('facebook')} round borderRadius={25} size={45} />
-        <WhatsappIcon className="cursor-pointer icon" onClick={()=> alert('whatsapp')} round borderRadius={25} size={45} />
-        <a aria-label={'Share on twitter'} href={`https://twitter.com/intent/tweet?text=${description}`} rel="noreferrer" target="_blank" data-size="large">
+        <a aria-label={'Share on facebook'} href={"https://www.facebook.com/sharer/sharer.php?u=link_to_be_shared" + (typeof window !== 'undefined' && window.location.href)} rel="noreferrer" target="_blank" data-action="share/facebook/share">
+        <FacebookIcon className="cursor-pointer icon" round borderRadius={25} size={45} />
+        </a>
+        <a aria-label={'Share on whatsapp'} href={"whatsapp://send?text=" + (typeof window !== 'undefined' && window.location.href)} rel="noreferrer" target="_blank" data-action="share/whatsapp/share">
+        <WhatsappIcon className="cursor-pointer icon" round borderRadius={25} size={45} />
+        </a>
+        <a aria-label={'Share on twitter'} href={`https://twitter.com/intent/tweet?text=${(typeof window !== 'undefined' && window.location.href)}`} rel="noreferrer" target="_blank" data-size="large">
             <TwitterIcon className="cursor-pointer icon" round borderRadius={25} size={45} >
         </TwitterIcon>
         </a>
+        <a aria-label={'Share on telegram'} href={`https://t.me/share/url?url=${(typeof window !== 'undefined' && window.location.href)}&text=${(description)}`} rel="noreferrer" target="_blank" data-size="large">
+
+        <TelegramIcon className="cursor-pointer icon" round borderRadius={25} size={45}></TelegramIcon>
+        </a>
+
     </div>
 
 }
