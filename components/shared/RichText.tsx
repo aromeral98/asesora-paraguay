45feebe87248910a1046a832a8571beeca048e20
aromeral98 <romeralalex@gmail.com>
2024-01-01 19:06:39 +0100

    feat: removed unused code, improbved chat gpt creating articles

--- components/shared/RichText.tsx
+++ components/shared/RichText.tsx
@@ -10,7 +10,7 @@ interface RichTextProps {
 export default function RichText({ data }: RichTextProps) {
   // TODO: STYLE THE MARKDOWN
   return (
-    <section className="rich-text py-6">
+    <section className="rich-text py-2">
       <Markdown remarkPlugins={[remarkGfm]}>
         {data.body}
       </Markdown>
