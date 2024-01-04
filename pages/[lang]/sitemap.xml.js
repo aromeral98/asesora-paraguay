5d7436f90db79c75fe5a091ad4b3b7f9a1e62366
aromeral98 <romeralalex@gmail.com>
2024-01-04 21:18:55 +0100

    updated sitemap

--- pages/[lang]/sitemap.xml.js
+++ pages/[lang]/sitemap.xml.js
@@ -27,112 +27,154 @@ function generateSiteMap() {
   <priority>0.97</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/impact-of-immigration</loc>
-  <lastmod>2024-01-03T17:29:24.677Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/covid-19-impact-resilience</loc>
+  <lastmod>2024-01-04T20:02:37.916Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.96</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/lgtbiq-contributions</loc>
-  <lastmod>2024-01-03T17:24:38.152Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/budget-travel-worldwide</loc>
+  <lastmod>2024-01-04T20:01:39.958Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.95</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/layers-of-machismo</loc>
-  <lastmod>2024-01-03T17:22:27.402Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/unlocking-financial-success</loc>
+  <lastmod>2024-01-04T19:59:01.018Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.94</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/global-warming-crisis</loc>
-  <lastmod>2024-01-03T17:20:58.895Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/profound-journey-meditation</loc>
+  <lastmod>2024-01-04T19:55:44.986Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.93</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/understanding-drought</loc>
-  <lastmod>2024-01-03T17:19:09.187Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/transformative-power-of-travel</loc>
+  <lastmod>2024-01-04T19:42:58.037Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.92</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/navigating-climate-change</loc>
-  <lastmod>2024-01-03T17:19:53.426Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/naples-pizza-authentic-italian-delight</loc>
+  <lastmod>2024-01-04T19:42:01.079Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.91</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/exploring-ecology</loc>
-  <lastmod>2024-01-03T17:18:58.009Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/challenges-faced-by-lgbti-individuals</loc>
+  <lastmod>2024-01-03T20:39:04.530Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.90</priority>
   </url>
   <url>
-  <loc>https://www.asesoraparaguay.com/en/sweet-world-honey-bees</loc>
-  <lastmod>2024-01-03T17:00:01.138Z</lastmod>
+  <loc>https://www.asesoraparaguay.com/en/immigration-benefits</loc>
+  <lastmod>2024-01-04T17:48:38.385Z</lastmod>
   <changefreq>daily</changefreq>
   <priority>0.89</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/lgtbiq-contributions</loc>
+  <lastmod>2024-01-03T20:20:35.618Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.88</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/layers-of-machismo</loc>
+  <lastmod>2024-01-03T20:20:54.401Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.87</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/global-warming-crisis</loc>
+  <lastmod>2024-01-03T20:21:14.606Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.86</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/understanding-drought</loc>
+  <lastmod>2024-01-03T20:21:33.226Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.85</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/navigating-climate-change</loc>
+  <lastmod>2024-01-03T20:21:49.236Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.84</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/exploring-ecology</loc>
+  <lastmod>2024-01-04T18:30:52.727Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.83</priority>
+  </url>
+  <url>
+  <loc>https://www.asesoraparaguay.com/en/sweet-world-honey-bees</loc>
+  <lastmod>2024-01-04T17:46:47.963Z</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.82</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/paraguayan-palate-pleaser-exploring-the-delights-of-sopa-paraguaya</loc>
   <lastmod>2023-12-26T11:04:13.967Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.88</priority>
+  <priority>0.81</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/savoring-tradition-exploring-paraguay-s-mbeju-a-cheesy-culinary-delight</loc>
   <lastmod>2023-12-26T10:59:47.926Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.87</priority>
+  <priority>0.80</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/sizzling-mastery-unveiling-the-art-of-paraguayan-asado-grilling-perfection</loc>
   <lastmod>2023-12-26T11:00:02.479Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.86</priority>
+  <priority>0.79</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/savoring-tradition-a-delectable-guide-to-chipa-paraguaya-paraguay-s-culinary-gem</loc>
   <lastmod>2024-01-03T17:13:41.123Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.85</priority>
+  <priority>0.78</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/paraguay-cuisine-discover-traditional-dishes-and-foods-in-paraguay</loc>
   <lastmod>2024-01-03T17:05:31.838Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.84</priority>
+  <priority>0.77</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/fluvial-fleets-of-china-russia-the-united-states-brazil-and-paraguay</loc>
   <lastmod>2024-01-03T17:02:23.186Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.83</priority>
+  <priority>0.76</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/exploring-the-culinary-delight-of-vori-vori-in-paraguayan-cuisine</loc>
   <lastmod>2024-01-03T17:02:37.959Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.82</priority>
+  <priority>0.75</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/discover-the-best-places-to-visit-in-paraguay-a-guide-to-tourism-in-paraguay</loc>
   <lastmod>2024-01-03T17:12:01.134Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.81</priority>
+  <priority>0.74</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/advantages-of-paraguay-s-river-market-for-importing-or-exporting</loc>
   <lastmod>2024-01-03T17:05:08.146Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.80</priority>
+  <priority>0.73</priority>
   </url>
   <url>
   <loc>https://www.asesoraparaguay.com/en/a-haven-for-digital-nomads-how-paraguay-attracts-digital-nomads</loc>
   <lastmod>2024-01-03T17:04:46.366Z</lastmod>
   <changefreq>daily</changefreq>
-  <priority>0.79</priority>
+  <priority>0.72</priority>
   </url>
   </urlset>`;
 
