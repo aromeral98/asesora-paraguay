58d7bfa63290131584b0be7a772242ca823ac937
Alejandro romeral <romeralalex@gmail.com>
2024-01-10 18:04:33 +0100

    removed redirects

--- next.config.js
+++ next.config.js
@@ -5,43 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 module.exports = withBundleAnalyzer({
   async redirects() {
-    return [
-      {
-        source: '/article',
-        destination: '/en/article',
-        permanent: true,
-      },
-      {
-        source: '/article/advantages',
-        destination: '/en/article/advantages-being-freelance-paraguay',
-        permanent: true,
-      },
-      {
-        source: '/article/tourism',
-        destination: '/en/article/tourism-paraguay-sites-visit',
-        permanent: true,
-      },
-      {
-        source: '/article/lowesttaxedcountries',
-        destination: '/en/article/low-taxed-countries-world',
-        permanent: true,
-      },
-      {
-        source: '/article/weather',
-        destination: '/en/article/climate-paraguay',
-        permanent: true,
-      },
-      {
-        source: '/freelance',
-        destination: '/move-to-paraguay-as-a-freelancer',
-        permanent: true,
-      },
-      {
-        source: '/advantages',
-        destination: '/en/article/advantages-being-freelance-paraguay',
-        permanent: true,
-      },
-    ];
+    return []
   },
   i18n: {
     locales: ['es'],
